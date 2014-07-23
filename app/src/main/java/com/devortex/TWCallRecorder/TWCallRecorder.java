c449cb09f8e7969a54e81ff498f8cc5c8396f0f8
Patrick L <patrick.lower@salientfed.com>
2014-07-23 16:58:20 -0400

    A bit of cleanup after experimentation Got a UI for settings to work

--- app/src/main/java/com/devortex/TWCallRecorder/TWCallRecorder.java
+++ app/src/main/java/com/devortex/TWCallRecorder/TWCallRecorder.java
@@ -1,43 +1,52 @@
-package com.devortex.xcallrecorder;
+package com.devortex.TWCallRecorder;
 
 /**
  * Created by Patrick.Lower on 7/18/2014.
  */
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
-public class main implements IXposedHookLoadPackage {
+public class TWCallRecorder implements IXposedHookZygoteInit, IXposedHookLoadPackage {
+    private static XSharedPreferences prefs;
 
     @Override
     public void handleLoadPackage(final LoadPackageParam loadPackageParam) throws Throwable {
         if (!loadPackageParam.packageName.equals("com.android.incallui"))
             return;
 
-        XposedBridge.log("Devortex: Entered Mod!");
+        final boolean enabled = prefs.getBoolean("enable", false);
+
         findAndHookMethod("com.android.services.telephony.common.PhoneFeature", loadPackageParam.classLoader, "makeFeatureForCommon", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("Devortex: inside hook");
+
+        @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 Class<?> phoneFeature = XposedHelpers.findClass("com.android.services.telephony.common.PhoneFeature", loadPackageParam.classLoader);
                 HashMap<String, Boolean> mFeatureList = (HashMap<String, Boolean>) XposedHelpers.getStaticObjectField(phoneFeature, "mFeatureList");
                 if (mFeatureList != null) {
-                    mFeatureList.put("voice_call_recording", false); //disable record button in case its enabled
-                    mFeatureList.put("voice_call_recording_menu", true); //enable record in menu
+                    mFeatureList.put("voice_call_recording", enabled && prefs.getBoolean("show_button", false));
+                    mFeatureList.put("voice_call_recording_menu", enabled && prefs.getBoolean("show_menu", false));
                 } else {
-                    XposedBridge.log("Devortex: param is null");
+                    XposedBridge.log("TWCallRecorder: param is null");
                 }
             }
         });
+    }
 
-        XposedBridge.log("Devortex: Finished Mod!");
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        String packageName = TWCallRecorder.class.getPackage().getName();
+
+        prefs = new XSharedPreferences(packageName);
+        prefs.makeWorldReadable();
     }
 }
