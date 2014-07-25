fbc58852f161ee2e780e45af263969f6648d73a4
Patrick L <patrick.lower@salientfed.com>
2014-07-25 15:39:28 -0400

    Added support for Note 3 and maybe other Samsung devices..

--- app/src/main/java/com/devortex/TWCallRecorder/TWCallRecorder.java
+++ app/src/main/java/com/devortex/TWCallRecorder/TWCallRecorder.java
@@ -12,6 +12,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
@@ -21,25 +22,38 @@ public class TWCallRecorder implements IXposedHookZygoteInit, IXposedHookLoadPac
 
     @Override
     public void handleLoadPackage(final LoadPackageParam loadPackageParam) throws Throwable {
-        if (!loadPackageParam.packageName.equals("com.android.incallui"))
+        if (!loadPackageParam.packageName.equals("com.android.incallui") && !loadPackageParam.packageName.equals("com.android.phone"))
             return;
 
         final boolean enabled = prefs.getBoolean("enable", false);
+        if (loadPackageParam.packageName.equals("com.android.incallui")) {
+            findAndHookMethod("com.android.services.telephony.common.PhoneFeature", loadPackageParam.classLoader, "makeFeatureForCommon", new XC_MethodHook() {
 
-        findAndHookMethod("com.android.services.telephony.common.PhoneFeature", loadPackageParam.classLoader, "makeFeatureForCommon", new XC_MethodHook() {
-
-        @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                Class<?> phoneFeature = XposedHelpers.findClass("com.android.services.telephony.common.PhoneFeature", loadPackageParam.classLoader);
-                HashMap<String, Boolean> mFeatureList = (HashMap<String, Boolean>) XposedHelpers.getStaticObjectField(phoneFeature, "mFeatureList");
-                if (mFeatureList != null) {
-                    mFeatureList.put("voice_call_recording", enabled && prefs.getBoolean("show_button", false));
-                    mFeatureList.put("voice_call_recording_menu", enabled && prefs.getBoolean("show_menu", false));
-                } else {
-                    XposedBridge.log("TWCallRecorder: param is null");
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    doMod(loadPackageParam, enabled);
                 }
-            }
-        });
+            });
+        } else if (loadPackageParam.packageName.equals("com.android.phone")) {
+            findAndHookMethod("com.android.phone.PhoneFeature", loadPackageParam.classLoader, "makeFeatureForCommon", new XC_MethodHook() {
+
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    doMod(loadPackageParam, enabled);
+                }
+            });
+        }
+    }
+
+    private void doMod(LoadPackageParam loadPackageParam, boolean enabled) {
+        Class<?> phoneFeature = XposedHelpers.findClass("com.android.services.telephony.common.PhoneFeature", loadPackageParam.classLoader);
+        HashMap<String, Boolean> mFeatureList = (HashMap<String, Boolean>) XposedHelpers.getStaticObjectField(phoneFeature, "mFeatureList");
+        if (mFeatureList != null) {
+            mFeatureList.put("voice_call_recording", enabled && prefs.getBoolean("show_button", false));
+            mFeatureList.put("voice_call_recording_menu", enabled && prefs.getBoolean("show_menu", false));
+        } else {
+            XposedBridge.log("TWCallRecorder: param is null");
+        }
     }
 
     @Override
